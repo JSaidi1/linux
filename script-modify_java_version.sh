cc6387871ee2cacafc13b80bfbd929300c58eed2
JSaidi1 <jabrane_saidi@outlook.com>
2025-03-29 16:40:22 +0100

    -script-modify_maven_version.sh: factorisation -script-modify_java_version.sh: no major modifications

--- script-modify_java_version.sh
+++ script-modify_java_version.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-#----------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 # Script Name: script-modify_java_version.sh
-# Description: This script automates the process of [insert task here].
+# Description: This script automates the process of installing a Java version based on a version folder.
 # Author: Jabrane SAIDI
 # Date: 2025-03-25
 # Dependencies: No dependencies
-#----------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 #set -x
 
 ## Define variables
 PATH_OF_ENV_FILE=~/.bashrc
 NAME_OF_ENV_FILE="the environment variables file"
 
-## Function to check if a directory exists
-check_directory() {
-    if [ ! -d "$1" ]; then
-        echo -e "\nError: Directory '$1' does not exists."
-        exit 1
+## Function to check current Java version (S)
+check_current_java_version() {
+    if command -v java &>/dev/null; then
+        local java_version=$(java -version 2>&1 | head -n 1)
+        echo -e "\nJava is already installed: $java_version."
+    else
+        echo -e "\nCurrently, Java is not installed."
     fi
 }
 
-## Function to check if a file exists ($1 name of env. file - $2 path to path)
+## Function to check if a file exists (G)
 check_file() {
     if [ ! -f "$2" ]; then
         echo -e "\nError: File ($1 '$2') does not exist."
@@ -29,45 +31,7 @@ check_file() {
     fi
 }
 
-## Function to check current java version
-check_current_java_version() {
-    if command -v java &>/dev/null; then
-        local java_version=$(java -version 2>&1 | head -n 1)
-        echo -e "\nInfo: Java is already installed: $java_version."
-    else
-        echo -e "\nInfo: Currently, Java is not installed."
-    fi
-}
-
-check_user_response() {
-    local exit_string="exit"
-    if [[ "${1,,}" == "${exit_string,,}" ]]; then
-        echo -e "\nYou have chosen to exit. See you, bye!"
-        exit 0
-    fi
-}
-
-## Function to Check if a java folder contains a 'bin/java' executable:
-check_java_executable() {
-    if [ -f "$1/bin/java" ]; then
-        # Run 'java -version' from the folder's bin directory to get the Java version:
-        local version_output=$("$1/bin/java" -version 2>&1)
-  
-        if [[ "$version_output" == *"version"* ]]; then
-            echo -e "\nThe folder of the new Java version '$1' contains a valid Java version:"
-            echo "$version_output"
-            # Processing continu...
-        else
-            echo -e "\nError: This folder does not seem to contain a valid Java installation."
-            exit 1
-        fi
-    else
-        echo -e "\nError: No Java executable found in this folder ($1)."
-        exit 1
-    fi
-}
-
-## Function to tell user that Java variable will be defined in the env. file (PATH_OF_ENV_FILE) and ask for confirmation
+## Function to tell user that Java variable will be defined in the env. file (PATH_OF_ENV_FILE) and ask for confirmation (S)
 ask_user_to_validate_env_file(){
     while true; do
         read -p $'\nNew Java version variables will be defined in the environment file '$1'. Do you want to continue? (y/n): ' user_response
@@ -87,6 +51,44 @@ ask_user_to_validate_env_file(){
     done
 }
 
+## Function to check user response (G)
+check_user_response() {
+    local exit_string="exit"
+    if [[ "${1,,}" == "${exit_string,,}" ]]; then
+        echo -e "\nYou have chosen to exit. See you, bye!"
+        exit 0
+    fi
+}
+
+## Function to check if a directory exists (G)
+check_directory() {
+    if [ ! -d "$1" ]; then
+        echo -e "\nError: Directory '$1' does not exists."
+        exit 1
+    fi
+}
+
+## Function to Check if a Java folder contains a 'bin/java' executable (S)
+check_java_executable() {
+    if [ -f "$1/bin/java" ]; then
+        # Run 'java -version' from the folder's bin directory to get the Java version:
+        local version_output=$("$1/bin/java" -version 2>&1)
+  
+        if [[ "$version_output" == *"version"* ]]; then
+            echo -e "\nThe folder of the new Java version '$1' contains a valid Java version:"
+            echo "$version_output"
+            # Processing continu...
+        else
+            echo -e "\nError: This folder does not seem to contain a valid Java installation."
+            exit 1
+        fi
+    else
+        echo -e "\nError: No Java executable found in this folder ($1)."
+        exit 1
+    fi
+}
+
+# Function to prepare env. file (S)
 prepare_env_file() {
     # Backup the env. file
     cp $1 "$1.bak"
@@ -100,6 +102,7 @@ prepare_env_file() {
     sed -i '/PATH=\$JAVA_HOME\/bin/d' $1
 }
 
+# Function to write the new Java config. in env. file (S)
 write_new_java_config() {
     # Add comment "# Add JAVA_HOME variable (<currentdate>)":
     echo "# Add JAVA_HOME variable (date=$(date "+%d-%m-%Y")):" | tee -a $1 > /dev/null
@@ -113,19 +116,21 @@ write_new_java_config() {
     echo -e "The line 'export PATH=\$JAVA_HOME/bin:\$PATH' was added to the end of the file '$1'.\n"    
 }
 
+# Function to ask user to reboot to apply changes on env. variables file (S)
 ask_user_to_reboot_to_apply_changes() {
     read -p "To be sure that the new Java version was successfully installed, the machine must restart. Do you want to restart it? (y/n): " user_answer
     if [[ "$user_answer" == "y" || "$user_answer" == "Y" ]]; then
         systemctl reboot -i
     else
         echo -e "\n/!\ Warning: in some cases, the new Java version will not be installed until you restart your machine!"
+        exit 0
     fi
 }
 
 # Main function 
 main() {
 
-    # Check java version already installed (if exists)
+    # Check Java version already installed (if exists)
     check_current_java_version
 
     # Check if the env. file exists
--- script-modify_maven_version.sh
+++ script-modify_maven_version.sh
@@ -1,145 +1,172 @@
 
 #!/bin/bash
 
-#-----------------------------
-# Date: 22-03-2025
-# Realized by: Jabrane SAIDI
-# Bash version: 5.2.21
-#-----------------------------
+#-----------------------------------------------------------------------------------------------------------
+# Script Name: script-modify_maven_version.sh
+# Description: This script automates the process of installing a Maven version based on a version folder.
+# Author: Jabrane SAIDI
+# Date: 2025-03-25
+# Dependencies: No dependencies
+#-----------------------------------------------------------------------------------------------------------
+#set -x
 
-#-----------------------------
-# Environment Preparation:
-# For the script to work, you must:
-# -Have the maven folders with their real names that reflect the real maven version
-# -These directories must be in the same folder. For example, in the .../maven folder:
-#       .../maven/maven-3.9.9 
-#       .../maven/maven-3.8.8
-#       ...
-## -All maven versions must me added to 'update-altarnatives'
-#-----------------------------
+## Define variables
+PATH_OF_ENV_FILE=~/.bashrc
+NAME_OF_ENV_FILE="the environment variables file"
 
-#set -x 
+## Function to check current Maven version (S)
+check_current_maven_version(){
+    if command -v mvn &> /dev/null
+    then
+        echo -e "\nMaven is already installed, version: $(mvn -v | grep 'Apache Maven' | awk '{print $3}')"
+    else
+        echo -e "\nCurrently, Maven is not installed."
+    fi
+}
 
-maven_versions_dir="/opt/build_automation_tools/maven/" # /!\: must always be absolute and have / at the end
-start_string_to_extract_maven_version=$maven_versions_dir
-end_string_to_extract_maven_version="/bin/mvn" # /!\: must always Not have / at the end
-choice_word_to_exit="Exit"
-current_maven_version=$(mvn -v | head -n 1 | awk '{print $3}') # before modifying it
-maven_home_file="/etc/environment"
-declare -A choice_nbr_of_maven_version_array # choice_nbr_of_maven_version_array[choice_nbr_of_maven_version]
+## Function to check if a file exists (G)
+check_file() {
+    if [ ! -f "$2" ]; then
+        echo -e "\nError: File ($1 '$2') does not exist."
+        exit 1
+    fi
+}
 
-#== verify if maven_versions_dir exists and it's not empty ==#
-if [ -d "$maven_versions_dir" ] && [ "$(ls -A $maven_versions_dir)" ]; then
-    #echo "Directory exists and is not empty."
-    
-    ## List all maven versions 
-    output=($(sudo update-alternatives --display mvn | grep priority))
-    
-    #== Display the menu for the user:
-    echo -e "Please select a version of maven to activate by entering the corresponding number:\n"
-
-    choice_nbr_of_maven_version=1
-
-    # Print all elements of the array
-    for item in "${output[@]}"
-    do
-        if [[ "$item" == *"bin/mvn"* ]]; then
-             
-            maven_version=$(echo "$item" | grep -oP "(?<=${start_string_to_extract_maven_version})(.*?)(?=${end_string_to_extract_maven_version})")
-            #maven_versions_array[$maven_version]="$item"           
-
-            if [[ "$maven_version" == *"$current_maven_version"* ]]; then
-                #echo "String '$current_maven_version' is present in '$maven_version'"
-                echo "$choice_nbr_of_maven_version: $maven_version (* current version)"
-            else
-                #echo "String '$current_maven_version' is not present in '$maven_version'"
-                echo "$choice_nbr_of_maven_version: $maven_version"
-            fi
-            
-            choice_nbr_of_maven_version_array[$choice_nbr_of_maven_version]=$maven_version
-            
-            choice_nbr_of_maven_version=$((choice_nbr_of_maven_version + 1))           
-
- 	    fi
-    done   
-    
-    ##== Add Exit to the list as last choice: 
-    echo "$choice_nbr_of_maven_version: $choice_word_to_exit"
-
-    ##== Add x: Exit to choice_nbr_of_maven_version_array:
-    choice_nbr_of_maven_version_array[$choice_nbr_of_maven_version]=$choice_word_to_exit
-
-    ##==test choice_nbr_of_maven_version_array:
-    ## Loop through the associative array and print key-value pairs
-    #echo "============ Test array:"
-    #for key in "${!choice_nbr_of_maven_version_array[@]}"; do
-    #    echo "$key: ${choice_nbr_of_maven_version_array[$key]}"
-    #done
-
+## Function to tell user that Maven variable will be defined in the env. file (PATH_OF_ENV_FILE) and ask for confirmation (S)
+ask_user_to_validate_env_file(){
     while true; do
-        ##== Prompt for user input:
-        echo -e "\n"
-        read -p "Enter your choice number from the list: " user_choice
+        read -p $'\nNew Maven version variables will be defined in the environment file '$1'. Do you want to continue? (y/n): ' user_response
+        
+        user_response="${user_response,,}"
 
-        ##== Process the user's choice:
-        ## Check if the choice exists as key in choice_nbr_of_maven_version_array:
-        if [[ -v choice_nbr_of_maven_version_array["$user_choice"] ]]; then
-            #echo "$choice is a key in the array."
-            echo "You chose '$user_choice: ${choice_nbr_of_maven_version_array[$user_choice]}'"
-
-            ##== Case of Exit choice:
-            if [[ "${choice_nbr_of_maven_version_array[$user_choice]}" == "$choice_word_to_exit" ]]; then
-                echo "Bye!"
-                break;
-            ##== Case of maven version already installed:
-            elif [[ "${choice_nbr_of_maven_version_array[$user_choice]}" == *"$current_maven_version"* ]]; then
-                echo -e "\n- Already installed (it is the current maven version)!"
-                break;
-            else
-                #echo "Continu processing..."
-                ##== Switch the system to the new maven version:
-                sudo update-alternatives --set mvn "$maven_versions_dir${choice_nbr_of_maven_version_array[$user_choice]}/bin/mvn"
-
-                ##== Check the new version of maven:
-                new_maven_version=`mvn -v | head -n 1 | awk '{print $3}'`
-
-                ##== Msg to user to confirm that maven version was changed:
-                echo -e "\n- Old maven version($current_maven_version) => New maven version($new_maven_version)\n"
-
-                ##== Set $maven_HOME on the file maven_home_file:
-                    ## Backup /etc/environment file
-                    sudo cp $maven_home_file "$maven_home_file.bak"
-                    
-                    ## Delete lines which containes maven_HOME:
-                sudo sed -i '/MAVEN_HOME/d' $maven_home_file
-                    
-                    ## Add on the end of the file $maven_HOME:
-                echo "MAVEN_HOME=\"$maven_versions_dir${choice_nbr_of_maven_version_array[$user_choice]}\"" | sudo tee -a $maven_home_file > /dev/null
-                    
-                    ## Update $maven_home_file:
-                echo -e "- The line 'MAVEN_HOME=$maven_versions_dir${choice_nbr_of_maven_version_array[$user_choice]}' was added to the end of the file '$maven_home_file'.\n"
-                    
-                    ## Ask user to restart session to apply environment variable modification:
-                read -p "- To apply environment variable changes (MAVEN_HOME), the machine must restart. Do you want to restart it? (y/n): " user_answer
-                    
-                if [[ "$user_answer" == "y" || "$user_answer" == "Y" ]]; then
-                    ## Restart system:
-                    systemctl reboot -i
-                else
-                    echo -e "\n/!\ Warning: MAVEN_HOME variable can't take its new value until you restart the machine!"
-                fi
-
-                break;
-            fi
-
-            
+        if [[ "$user_response" == "y" ]]; then # Continu processing ...
+            break
+        elif [[ "$user_response" == "n" ]]; then
+            echo -e "\nYour choice is 'no'. See you, bye!"
+            echo "N.B: If you want to define an other environment file than '$1', you can define it in 'PATH_OF_ENV_FILE' variable."
+            exit 0
         else
-            echo "'$user_choice' is not a number in the list. Please try again: "
+            echo "Please answer with 'yes' or 'no'."
         fi
 
-    done #while
+    done
+}
 
+## Function to check user response (G)
+check_user_response() {
+    local exit_string="exit"
+    if [[ "${1,,}" == "${exit_string,,}" ]]; then
+        echo -e "\nYou have chosen to exit. See you, bye!"
+        exit 0
+    fi
+}
+
+## Function to check if a directory exists (G)
+check_directory() {
+    if [ ! -d "$1" ]; then
+        echo -e "\nError: Directory '$1' does not exists."
+        exit 1
+    fi
+}
+
+## Function to Check if a Maven folder contains a 'bin/java' executable (S)
+check_maven_executable() {
+    if [ -f "$1/bin/mvn" ]; then
+        # Run 'mvn -version' from the folder's bin directory to get the Maven version:
+        local version_output=$("$1/bin/mvn" -version 2>&1)
+  
+        if [[ "$version_output" == *"version"* ]]; then
+            echo -e "\nThe folder of the new Maven version '$1' contains a valid Maven version:"
+            echo "$version_output"
+            # Processing continu...
+        else
+            echo -e "\nError: This folder does not seem to contain a valid Maven installation."
+            exit 1
+        fi
+    else
+        echo -e "\nError: No Maven executable found in this folder ($1)."
+        exit 1
+    fi
+}
+
+# Function to prepare env. file (S)
+prepare_env_file() {
+    # Backup the env. file
+    cp $1 "$1.bak"
+    # Delete spaces before and after the = sign on lines that contain "MAVEN_HOME" string
+    sed -i '/MAVEN_HOME/ s/ *= */=/g' $1
+    # Delete spaces before and after the = sign on lines that contain "M2_HOME" string
+    sed -i '/M2_HOME/ s/ *= */=/g' $1
+    # Delete comments which contains "Add MAVEN_HOME variable"
+    sed -i '/^#.*Add MAVEN_HOME variable/d' $1
+    # Delete lines which contains "MAVEN_HOME="
+    sed -i '/MAVEN_HOME=/d' $1
+    # Delete lines which contains "M2_HOME="
+    sed -i '/M2_HOME=/d' $1
+    # Delete lines which contains "PATH=$MAVEN_HOME/bin"
+    sed -i '/PATH=\$MAVEN_HOME\/bin/d' $1
+}
+
+# Function to write the new Maven config. in env. file (S)
+write_new_maven_config() {
+    # Add comment "# Add MAVEN_HOME variable (<currentdate>)":
+    echo "# Add MAVEN_HOME variable (date=$(date "+%d-%m-%Y")):" | tee -a $1 > /dev/null
+    # Add export MAVEN_HOME=...:
+    echo "export MAVEN_HOME=$2" | tee -a $1 > /dev/null
+    # Tell the user that the line export MAVEN_HOME=... was added to the file:
+    echo -e "\nThe line 'export MAVEN_HOME=$2' was added to the end of the file '$1'.\n"
+    # Add export M2_HOME=...:
+    echo "export M2_HOME=$2" | tee -a $1 > /dev/null
+    # Tell the user that the line export M2_HOME=... was added to the file:
+    echo -e "The line 'export M2_HOME=$2' was added to the end of the file '$1'.\n"
+    # Add export PATH=...:
+    echo "export PATH=\$MAVEN_HOME/bin:\$PATH" | tee -a $1 > /dev/null
+    # Tell the user that the line export PATH=... was added to the file:
+    echo -e "The line 'export PATH=\$MAVEN_HOME/bin:\$PATH' was added to the end of the file '$1'.\n"    
+}
+
+# Function to ask user to reboot to apply changes on env. variables file (S)
+ask_user_to_reboot_to_apply_changes() {
+    read -p "To be sure that the new Maven version was successfully installed, the machine must restart. Do you want to restart it? (y/n): " user_answer
+    if [[ "$user_answer" == "y" || "$user_answer" == "Y" ]]; then
+        systemctl reboot -i
+    else
+        echo -e "\n/!\ Warning: in some cases, the new Maven version will not be installed until you restart your machine!"
+        exit 0
+    fi
+}
+
+# Main function 
+main() {
+
+    # Check Maven version already installed (if exists)
+    check_current_maven_version
+
+    # Check if the env. file exists
+    check_file "$NAME_OF_ENV_FILE" "$PATH_OF_ENV_FILE"
+
+    # Tell the user that Maven variable will be defined in the env. file (PATH_OF_ENV_FILE) and ask for confirmation
+    ask_user_to_validate_env_file "$PATH_OF_ENV_FILE"
+
+    # Prompt the user to set the path of Maven folder
+    read -p $'\nEnter the path of the new Maven directory to install the new Maven version (or set exit to terminate the script): ' PATH_OF_MAVEN_DIR
+    check_user_response "$PATH_OF_MAVEN_DIR"
+
+    # Check if the directory exists
+    check_directory "$PATH_OF_MAVEN_DIR"
+
+    # Check if the folder is a valid Maven version folder
+    check_maven_executable "$PATH_OF_MAVEN_DIR"
     
-else
-    echo "Error: directory '$maven_versions_dir' does not exist or is empty. Please set the right path to the variable 'maven_versions_dir'."
-fi
+    # Prepare env. file (and delete old Maven config version to write the config of the new one in the next step)
+    prepare_env_file "$PATH_OF_ENV_FILE"
+
+    # Write new Maven config. on the end of env. file
+    write_new_maven_config "$PATH_OF_ENV_FILE" "$PATH_OF_MAVEN_DIR"
+
+    # Ask the user to reboot session to apply environment variable modification
+    ask_user_to_reboot_to_apply_changes            
+}
+
+# Call main function
+main
